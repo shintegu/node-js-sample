00ff76e38b6d4c38e062037980c714c9f5ccf9ae
Nguyễn Minh Quân <quan1910@hotmail.com>
2018-05-02 16:05:54 +0700

    First commit

---
+++ constant.js
@@ -0,0 +1,6 @@
+exports.DB_NAME = "doyouknow_db";
+exports.DB_USERNAME = "doyouknow";
+exports.DB_PASSWORD = "123";
+exports.DB_HOST = "localhost";
+exports.DB_PORT = "5432";
+exports.DB_SCHEMA = "doyouknow_db";
--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 var express = require('express')
+var pg = require('pg');
 var app = express()
+var cors = require('cors')
+var CONSTANT = require('./constant.js')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.use(cors())
+
+
+var config = {
+  user: CONSTANT.DB_USERNAME,
+  database: CONSTANT.DB_NAME,
+  host: CONSTANT.DB_HOST,
+  password: CONSTANT.DB_PASSWORD,
+  port: CONSTANT.DB_PORT
+};
+
+console.log("config : " + JSON.stringify(config));
+
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+
+  response.send("<3 Hello <3");
+})
+
+app.get('/fact', function (request, response) {
+
+  var pool = new pg.Pool(config);
+  pool.query('select * from fact order by random() limit 1;', function (err, res) {
+    if (err) throw err
+    response.send(res.rows[0]);
+  })
 })
 
 app.listen(app.get('port'), function() {
