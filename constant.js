f451a57d20dbe823d26d64f50d6943a38e6e97aa
Nguyễn Minh Quân <quan1910@hotmail.com>
2018-05-02 16:54:57 +0700

    make it better

--- constant.js
+++ constant.js
@@ -1,6 +1,6 @@
-exports.DB_NAME = "doyouknow_db";
-exports.DB_USERNAME = "doyouknow";
-exports.DB_PASSWORD = "123";
-exports.DB_HOST = "localhost";
+exports.DB_NAME = "dbkg8io5urom3t";
+exports.DB_USERNAME = "etzjywzhzfexjx";
+exports.DB_PASSWORD = "6d58c44de4099804463eddd23de9020e3b3baf62cb606ec6f1bee43f03aa772e";
+exports.DB_HOST = "ec2-174-129-33-29.compute-1.amazonaws.com";
 exports.DB_PORT = "5432";
 exports.DB_SCHEMA = "doyouknow_db";
